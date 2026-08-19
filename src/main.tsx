986185d9e561bf3d6e6c228813262c37afe157df
Michał Skrzyński <michal.skrzynski@stylefect.com>
2026-08-19 23:28:00 +0200

    optimization of the number of fonts

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,17 @@ import ReactDOM from 'react-dom/client';
 import {App} from '@/app/App';
 import {Providers} from '@/app/providers';
 import '@/i18n/config';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/700.css';
-import '@fontsource/inter/800.css';
+import '@fontsource/inter/latin-400.css';
+import '@fontsource/inter/latin-ext-400.css';
 
-import '@fontsource/jetbrains-mono/600.css';
+import '@fontsource/inter/latin-700.css';
+import '@fontsource/inter/latin-ext-700.css';
+
+import '@fontsource/inter/latin-800.css';
+import '@fontsource/inter/latin-ext-800.css';
+
+import '@fontsource/jetbrains-mono/latin-600.css';
+import '@fontsource/jetbrains-mono/latin-ext-600.css';
 import '@/styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
