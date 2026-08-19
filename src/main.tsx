219e6bbeebc67944454a6bdc0049706e646aa1e5
Michał Skrzyński <michal.skrzynski@stylefect.com>
2026-08-19 23:11:53 +0200

    removing font weights 500 and 600

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,6 @@ import {App} from '@/app/App';
 import {Providers} from '@/app/providers';
 import '@/i18n/config';
 import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/inter/800.css';
 
